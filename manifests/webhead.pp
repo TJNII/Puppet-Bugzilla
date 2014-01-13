f77c3220546e629c9ce35e612b46dab7bcdacad0
Tom Noonan II <tom@tjnii.com>
2014-01-12 19:57:14 -0600

    Resolved SSL config error and added Apache2 notify

--- manifests/webhead.pp
+++ manifests/webhead.pp
@@ -65,6 +65,7 @@ class bugzilla::webhead (
       ensure  => link,
       target  => "/etc/apache2/sites-available/bugzilla",
       require => File['/etc/apache2/sites-available/bugzilla'],
+      notify  => Service['apache2'],
     }
 
     # Disable the "whineatnews.pl" script to curtail whining
